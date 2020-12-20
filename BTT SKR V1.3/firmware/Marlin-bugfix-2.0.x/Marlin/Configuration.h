6527bc152903b7868df760ecfdab6955376f61d2
RobMackie <rob.mackie@gmail.com>
2020-12-20 03:03:48 -0500

    tested z endstop by hand. needs to be inverted as done in this commit. also - don'tknow that it matters but commented out the z probe option since there is no z probe
     Changes to be committed:
    	modified:   BTT SKR V1.3/firmware/Marlin-bugfix-2.0.x/Marlin/Configuration.h

--- BTT SKR V1.3/firmware/Marlin-bugfix-2.0.x/Marlin/Configuration.h
+++ BTT SKR V1.3/firmware/Marlin-bugfix-2.0.x/Marlin/Configuration.h
@@ -639,7 +639,7 @@
 // Mechanical endstop with COM to ground and NC to Signal uses "false" here (most common setup).
 #define X_MIN_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
 #define Y_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define Z_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
+#define Z_MIN_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
 #define X_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define Y_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define Z_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
@@ -813,7 +813,7 @@
  *
  * Enable this option for a probe connected to the Z Min endstop pin.
  */
-#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
+// #define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
 
 /**
  * Z_MIN_PROBE_PIN
